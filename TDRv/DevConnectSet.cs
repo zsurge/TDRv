e9e43fb8abb3eb300fa1cfa8a900a1d52f7aba5a
zdsurge <zdsurge@163.com>
2022-07-25 17:53:49 +0800

    2022.07.25 1.E5063A-2H5   MY54705930和   MY54705910

--- TDRv/DevConnectSet.cs
+++ TDRv/DevConnectSet.cs
@@ -258,7 +258,7 @@ namespace TDRv
                 else if (sn.Contains("MY59201572"))
                 {
 
-                    if (202111071400 - Convert.ToInt64(DateTime.Now.ToString("yyyyMMddHHmm")) <= 0)
+                    if (209912301400 - Convert.ToInt64(DateTime.Now.ToString("yyyyMMddHHmm")) <= 0)
                     {
                         optStatus.isConnect = false;
                         combDevString.BackColor = Color.Red;
@@ -489,6 +489,28 @@ namespace TDRv
                 else if (sn.Contains("MY54705796"))
                 {
                     if (209912301400 - Convert.ToInt64(DateTime.Now.ToString("yyyyMMddHHmm")) <= 0)
+                    {
+                        optStatus.isConnect = false;
+                        combDevString.BackColor = Color.Red;
+                        return;
+                    }
+
+                    if (ret != 0)
+                    {
+                        optStatus.isConnect = false;
+                        combDevString.BackColor = Color.Red;
+                        MessageBox.Show("error!");
+                    }
+                    else
+                    {
+                        optStatus.isConnect = true;
+                        combDevString.BackColor = Color.Green;
+                    }
+                }
+                //add 2022.07.25
+                else if (sn.Contains("MY54705930") || sn.Contains("MY54705910"))
+                {
+                    if (202210251400 - Convert.ToInt64(DateTime.Now.ToString("yyyyMMddHHmm")) <= 0)
                     {
                         optStatus.isConnect = false;
                         combDevString.BackColor = Color.Red;
