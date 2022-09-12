44e92f4dd16f2b9f03abca01580ac72b94722914
zsurge <zdsurge@163.com>
2022-09-12 21:35:05 +0800

    V1.0.4.20220912 1.delay 2022.09.20

--- TDRv/DevConnectSet.cs
+++ TDRv/DevConnectSet.cs
@@ -43,7 +43,7 @@ namespace TDRv
 
             string NowDate = DateTime.Now.ToString("yyyyMMdd");
 
-            if (20220910 - Convert.ToInt32(DateTime.Now.ToString("yyyyMMdd")) <= 0)
+            if (20220920 - Convert.ToInt32(DateTime.Now.ToString("yyyyMMdd")) <= 0)
             {
                 optStatus.isConnect = false;
                 combDevString.BackColor = Color.Red;
