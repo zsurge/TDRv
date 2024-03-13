95f9eda4a9576aacc6ab4943acb35c2bd9665c14
zdsurge <zdsurge@163.com>
2024-03-13 09:30:09 +0800

    MY54706013 delay 2024.04.13

--- TDRv/DevConnectSet.cs
+++ TDRv/DevConnectSet.cs
@@ -939,7 +939,7 @@ namespace TDRv
                 }
                 else if (sn.Contains("MY54706013"))
                 {
-                    if (202404111400 - Convert.ToInt64(DateTime.Now.ToString("yyyyMMddHHmm")) <= 0)
+                    if (202404131400 - Convert.ToInt64(DateTime.Now.ToString("yyyyMMddHHmm")) <= 0)
                     {
                         optStatus.isConnect = false;
                         combDevString.BackColor = Color.Red;
