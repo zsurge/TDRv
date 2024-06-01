ff843ed11b26857ce29b3e62c42e0eb8aadcdf39
surge <zdsurge@163.com>
2024-06-01 09:36:05 +0800

    1.add MY54706013 to 2099

--- TDRv/Form1.Designer.cs
+++ TDRv/Form1.Designer.cs
@@ -267,6 +267,7 @@
             // 
             // tsb_Pnl_ID
             // 
+            this.tsb_Pnl_ID.Font = new System.Drawing.Font("Microsoft YaHei UI", 9F);
             this.tsb_Pnl_ID.Name = "tsb_Pnl_ID";
             this.tsb_Pnl_ID.Size = new System.Drawing.Size(200, 39);
             this.tsb_Pnl_ID.Text = "0001";
@@ -279,6 +280,7 @@
             // 
             // tsb_Set_id
             // 
+            this.tsb_Set_id.Font = new System.Drawing.Font("Microsoft YaHei UI", 9F);
             this.tsb_Set_id.Name = "tsb_Set_id";
             this.tsb_Set_id.Size = new System.Drawing.Size(200, 39);
             // 
@@ -911,7 +913,7 @@
             this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
             this.KeyPreview = true;
             this.Name = "Form1";
-            this.Text = "TDR Automatic Test System 泰仕捷科技有限公司 V1.0.4.20231017";
+            this.Text = "TDR Automatic Test System 泰仕捷科技有限公司 V1.0.4.20240601";
             this.FormClosed += new System.Windows.Forms.FormClosedEventHandler(this.Form1_FormClosed);
             this.Load += new System.EventHandler(this.Form1_Load);
             this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.Form1_KeyDown);
